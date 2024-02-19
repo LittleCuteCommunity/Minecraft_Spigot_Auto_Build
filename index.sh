4dbee9476078f094c0fd3f112d854cfdb68ab42a
薄荷牛奶萌 <101342651+MoeCinnamo@users.noreply.github.com>
2024-02-19 20:03:00 +0800

    Update and rename build.sh to index.sh

---
+++ index.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#!/bin/bash
+
+echo Download and Compress BungeeCord
+cd BungeeCord
+curl -O https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
+cd module
+curl -O https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-alert/target/cmd_alert.jar
+curl -O https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-find/target/cmd_find.jar
+curl -O https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-kick/target/cmd_kick.jar
+curl -O https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-list/target/cmd_list.jar
+curl -O https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-send/target/cmd_send.jar
+curl -O https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-server/target/cmd_server.jar
+curl -O https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/reconnect-yaml/target/reconnect_yaml.jar
+cd ..
+zip -q -r ../dist/BungeeCord.zip *
+
+echo Build Spigot
+set -euo pipefail
+buildNumber=`curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/buildNumber`
+my_number=`curl https://api.github.com/repos/LittleCuteCommunity/Minecraft_Spigot_Auto_Build/releases/latest | jq -r '.tag_name'`
+
+echo latest spigot build tools release Number: $buildNumber
+echo latest my_build number: $my_number
+
+# Some times because of network error, it may not be digital, then we exit.
+if [ -n "`echo ${my_number} | sed -n '/^[0-9][0-9]*$/p'`" -a -n "`echo ${buildNumber} | sed -n '/^[0-9][0-9]*$/p'`" ];then
+
+    if [ $my_number -eq $buildNumber ];then
+        exit 0
+    fi
+else
+    exit 0
+fi
+
+cd dist
+curl -O https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar -Xmx1024M BuildTools.jar
+echo -n $buildNumber > new_moe_build.txt
