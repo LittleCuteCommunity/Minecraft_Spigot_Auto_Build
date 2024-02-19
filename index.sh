db2b81acbc6178aac479af0a5b06ab25d59aae34
薄荷牛奶萌 <101342651+MoeCinnamo@users.noreply.github.com>
2024-02-19 18:51:19 +0800

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-echo Download and Compress BungeeCord
-wget -O ./BungeeCord/BungeeCord.jar https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
-wget -O ./BungeeCord/module/cmd_alert.jar https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-alert/target/cmd_alert.jar
-wget -O ./BungeeCord/module/cmd_find.jar https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-find/target/cmd_find.jar
-wget -O ./BungeeCord/module/cmd_kick.jar https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-kick/target/cmd_kick.jar
-wget -O ./BungeeCord/module/cmd_list.jar https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-list/target/cmd_list.jar
-wget -O ./BungeeCord/module/cmd_send.jar https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-send/target/cmd_send.jar
-wget -O ./BungeeCord/module/cmd_server.jar https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/cmd-server/target/cmd_server.jar
-wget -O ./BungeeCord/module/reconnect_yaml.jar https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/module/reconnect-yaml/target/reconnect_yaml.jar
-zip -q -r ./dist/BungeeCord.zip BungeeCord
-
 echo Build Spigot
 set -euo pipefail
 buildNumber=`curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/buildNumber`
