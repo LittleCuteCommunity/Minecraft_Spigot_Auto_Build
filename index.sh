e58543c3b323d84ad0dcbd46de16fb3d2e229d4e
薄荷牛奶萌 <101342651+MoeCinnamo@users.noreply.github.com>
2024-02-19 18:32:54 +0800

    Update index.sh

--- index.sh
+++ index.sh
@@ -37,7 +37,7 @@ mv spigot-*.jar ./dist/spigot-*.jar
 echo Upload
 if [ -f 'new_moe_build.txt' ];then
     TAGNAME=$(cat new_moe_build.txt)
-    NAME="$(for a in "./dist/*"; do echo $a; done)"
-    # hub release create $(for a in "./dist/*"; do echo -a $a; done) -m "$NAME" -t "master" "$TAGNAME"
-    gh release create "$TAGNAME" --target "master" --title "$NAME" "./dist/*"
+    NAME="$(for a in './dist/spigot-*.jar'; do echo $a; done)"
+    # hub release create $(for a in './dist/spigot-*.jar'; do echo -a $a; done) -m "$NAME" -t "master" "$TAGNAME"
+    gh release create "$TAGNAME" --target "master" --title "$NAME" './dist/*'
 fi
