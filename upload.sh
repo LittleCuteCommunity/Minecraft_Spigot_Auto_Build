0fcb0daf4081fa5c3915dc6fff1369dda6239ab1
薄荷牛奶萌 <101342651+MoeCinnamo@users.noreply.github.com>
2024-02-19 17:53:30 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 if [ -f 'new_build.txt' ];then
     TAGNAME=$(cat new_build.txt)
     NAME="$(for a in spigot-*.jar; do echo $a; done)"
-    # hub release create $(for a in spigot-*.jar; do echo -a $a; done) -m "$NAME" -t "main" "$TAGNAME"
-    gh release create "$TAGNAME" --target "main" --title "$NAME" spigot-*.jar
+    # hub release create $(for a in spigot-*.jar; do echo -a $a; done) -m "$NAME" -t "master" "$TAGNAME"
+    gh release create "$TAGNAME" --target "master" --title "$NAME" spigot-*.jar
 fi
