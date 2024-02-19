1d0528309f7f4ad0e41bad581cabfb52835ee62e
薄荷牛奶萌 <101342651+MoeCinnamo@users.noreply.github.com>
2024-02-19 18:04:20 +0800

    Delete upload.sh

--- upload.sh
+++
@@ -1,6 +0,0 @@
-if [ -f 'new_build.txt' ];then
-    TAGNAME=$(cat new_build.txt)
-    NAME="$(for a in spigot-*.jar; do echo $a; done)"
-    # hub release create $(for a in spigot-*.jar; do echo -a $a; done) -m "$NAME" -t "master" "$TAGNAME"
-    gh release create "$TAGNAME" --target "master" --title "$NAME" spigot-*.jar
-fi
